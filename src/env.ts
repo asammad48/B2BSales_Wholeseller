0d9a0cc8ec9bdad36822596115481ddb06df0531
Abdul Sammad <Abdul Sammad@DESKTOP-L7JHC1H>
2026-03-17 05:17:30 +0500

    Changes in env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export const env = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000'),
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || (typeof window !== 'undefined' ? window.location.origin : 'https://xm9cpxwr-51948.asse.devtunnels.ms'),
 };
